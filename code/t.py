58e03d1726ec369fb18c397809616d1e274eede8
jirayuwat12 <jirayuwat12@gmail.com>
2019-04-12 08:02:25 +0100

    Set_Zero

--- code/t.py
+++
@@ -1,3 +0,0 @@
-import sys
-
-print('LED '+sys.argv[1]+' is on')
--- input.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<h1>Hello</h1>
-<?php
-$num =$_GET['led'];
-echo shell_exec("python ./code/t.py $num");
-?>
-</body>
-</html>
