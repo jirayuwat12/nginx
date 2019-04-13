c170cf8a1a2c3a9e322896a5d4ccab150fa64b94
jirayuwat12 <jirayuwat12@gmail.com>
2019-04-13 15:44:08 +0700

    test_java

--- main.php
+++ main.php
@@ -6,12 +6,18 @@
 		<title>MAIN</title>
 		<meta charset="utf-8">
 		<link href="st1.css" rel="stylesheet" >
+		<script>
+			function Click(){
+				document.getElementById('btn1').value = 'clicked';
+			}
+		</script>
 	</head>
 	<body>
 		<div>
 			<h1> <?php echo "Hello, Mr. ".$row_user['username']; ?> </h1>
 			<br></br>
-			<input type="button">
+			<input type="button" id="btn1" value="click">
+			<br></br>
 			<a href="login/logout.php">Log Out</a>
 		</div>
 	</body>
