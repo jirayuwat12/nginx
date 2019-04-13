09518ecdfc0678726bd2e9b98147ded8a88fd725
jirayuwat12 <jirayuwat12@gmail.com>
2019-04-13 15:45:29 +0700

    test_java2

--- main.php
+++ main.php
@@ -16,7 +16,7 @@
 		<div>
 			<h1> <?php echo "Hello, Mr. ".$row_user['username']; ?> </h1>
 			<br></br>
-			<input type="button" id="btn1" value="click">
+			<input type="button" id="btn1" value="click" onclick="Click();" />
 			<br></br>
 			<a href="login/logout.php">Log Out</a>
 		</div>
