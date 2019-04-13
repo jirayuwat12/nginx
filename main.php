89dd04d1a5e798c650a80e305185e8c8db1c35b1
jirayuwat12 <jirayuwat12@gmail.com>
2019-04-13 15:30:48 +0700

    edit-1

--- main.php
+++ main.php
@@ -1,25 +1,17 @@
 <?php
-session_start();
-if(!isset($_SESSION['login_id'])){
-header("Location: login/login.php");
-}
-require 'login/connectdb.php';
-$user_id = $_SESSION['login_id'];
-$qry_use = "SELECT * FROM tb_login WHERE id='$user_id'";
-$result_user = mysqli_query($dbcon,$qry_use);
-if($result_user){
-	$row_user = mysqli_fetch_array($result_user,MYSQLI_ASSOC);
-}
+	require 'start.php';
 ?>
 <html>
-<body>
-<h1><?php
-echo "Hello, Mr. ".$row_user['username'];
-?></h1>
-<br></br>
-<a href="login/logout.php">Log Out</a>
-</body>
+	<head>
+		<title>MAIN</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1> <?php echo "Hello, Mr. ".$row_user['username']; ?> </h1>
+		<br></br>
+		<input type="button">
+		<a href="login/logout.php">Log Out</a>
+	</body>
 </html>
-<?php
-mysqli_close($dbcon);
-?>
+
+<?php mysqli_close($dbcon); ?>
---
+++ start.php
@@ -0,0 +1,13 @@
+<?php
+  session_start();
+  if(!isset($_SESSION['login_id'])){
+    header("Location: login/login.php");
+  }
+  require 'login/connectdb.php';
+  $user_id = $_SESSION['login_id'];
+  $qry_use = "SELECT * FROM tb_login WHERE id='$user_id'";
+  $result_user = mysqli_query($dbcon,$qry_use);
+  if($result_user){
+  	$row_user = mysqli_fetch_array($result_user,MYSQLI_ASSOC);
+  }
+?>
