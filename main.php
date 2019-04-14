aa03ff810303feb9b2d3e05828cf8b5cedb810dc
jirayuwat12 <jirayuwat12@gmail.com>
2019-04-14 12:30:26 +0700

    fix1

--- main.php
+++ main.php
@@ -16,22 +16,19 @@
 		    'menu main main main right right'
 		    'menu footer footer footer footer footer';
 			  grid-gap: 10px;
-			  background-color: #2196F3;
 			  padding: 10px;
 			}
 			.grid-container > div {
-			  background-color: rgba(255, 255, 255, 0.8);
 			  text-align: center;
 			  padding: 20px 0;
 			  font-size: 30px;
 			}
 		</style>
+		<link href="st1.css" rel="stylesheet">
 	</head>
 	<body>
-		<h1>Grid Layout</h1>
-		<p>This grid layout contains six columns and three rows:</p>
 		<div class="grid-container">
-		  <div class="item1">Header</div>
+		  <div class="item1">Main Control</div>
 		  <div class="item2">Menu</div>
 		  <div class="item3">Main</div>
 		  <div class="item4">Right</div>
