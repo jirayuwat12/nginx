3b41cad156f1e607afefc6bb496ae6a03282c36c
jirayuwat12 <jirayuwat12@gmail.com>
2019-04-14 13:20:43 +0700

    fix main.php

--- main.php
+++ main.php
@@ -14,9 +14,6 @@
 	  		grid-template-areas:
 	    	'header header header header header header'
 		    'menu main main main right right'
-				'menu main main main right right'
-				'menu main main main right right'
-				'menu main main main right right'
 		    'menu footer footer footer footer footer';
 			  grid-gap: 10px;
 			}
@@ -24,16 +21,40 @@
 			  text-align: center;
 			  font-size: 30px;
 			}
+      #menu{
+        width:10vw;
+        height:100vh;
+        border: 1px solid blue;
+      }
+      #head{
+        width:98vw;
+        height:16vh;
+        border:1px solid blue;
+      }
+      #main{
+        width:67vw;
+        height:74vh;
+        border:1px solid blue;
+      }
+      #right{
+        width:19vw;
+        height:74vh;
+        border:1px solid blue;
+      }
+      #foot{
+        width:87vw;
+        height:24vh;
+        border:1px solid blue;
+      }
 		</style>
-		<link href="st1.css" rel="stylesheet">
 	</head>
 	<body>
 		<div class="grid-container">
-		  <div class="item1">Main Control</div>
-		  <div class="item2">Menu</div>
-		  <div class="item3">Main</div>
-		  <div class="item4">Right</div>
-		  <div class="item5">Footer</div>
+		  <div class="item1" id="head"><br>Main Control</div>
+		  <div class="item2" id="menu">Menu</div>
+		  <div class="item3" id="main">Main</div>
+		  <div class="item4" id="right">Right</div>
+		  <div class="item5" id="foot">Footer</div>
 		</div>
 	</body>
 </html>
