06e6654e7d323895359678722ed60b8cf68933c0
jirayuwat12 <jirayuwat12@gmail.com>
2019-04-15 20:38:13 +0700

    logout

--- main.php
+++ main.php
@@ -1,3 +1,16 @@
 <?php
 	require 'start.php';
 ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+		<link rel="stylesheet" href="login/style.css">
+	</head>
+	<body>
+		<form class="box" action="logout.php" method="post">
+			<input type="submit" name="" value="Logout">
+		</form>
+	</body>
+</html>
