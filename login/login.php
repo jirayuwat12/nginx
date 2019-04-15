1ab4ff096ed89c44023049588fbacfd04beab35c
jirayuwat12 <jirayuwat12@gmail.com>
2019-04-15 12:18:37 +0700

    f1

--- login/login.php
+++ login/login.php
@@ -8,10 +8,9 @@
     <div>
       <form class="box"action="login_check.php" method="post">
         <h1>Login</h1>
-        <input type="text" name="username" placeholder="Username" required>
-        <input type="password" name="pass" placeholder="Password" required>
+        <input type="text" name="username" placeholder="Username" >
+        <input type="password" name="pass" placeholder="Password" >
         <input type="submit" value="Login">
-        <a href="frm_register.php" class="button">Regist</a>
       </form>
     </div>
   </body>
