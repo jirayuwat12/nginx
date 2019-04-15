8f83b822d1d53049ef8c333b4b861b30de45cb30
jirayuwat12 <jirayuwat12@gmail.com>
2019-04-15 10:45:54 +0700

    regist_css

--- login/frm_register.php
+++ login/frm_register.php
@@ -1,25 +1,21 @@
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Register</title>
-<link href="style.css" rel="stylesheet">
-</head>
-<body>
-<div>
-<h1>Registoration</h1>
-<form action="register.php" method="post">
-<label for="username">Username</label>
-<input type="text" name="username" required autofocus>
-<label for="pass">Password</label>
-<input type="password" name="pass" required>
-<label for="email">Email</label>
-<input type="email" name="email" placeholder="example@doamin.com">
-<br></br>
-<input type="submit" value="Regis">
-<br></br>
-<br></br>
-<a href="index.php">Home Page</a>
-</form>
-</div>
-</body>
+  <head>
+    <meta charset="UTF-8">
+    <title>Register</title>
+    <link href="style_login.css" rel="stylesheet">
+  </head>
+  <body>
+    <div>
+      <form class="box"action="register.php" method="post">
+        <h1>Registoration</h1>
+        <input type="text" name="username" placeholder="Username" required autofocus>
+        <input type="password" name="pass" placeholder="Password" required>
+        <input type="email" name="email" placeholder="example@doamin.com">
+        <input type="submit" value="Regis">
+        <form class="" action="index.php" method="post">
+          <input type="submit" name="" value="Home">
+        </form>
+      </form>
+    </div>
+  </body>
 </html>
